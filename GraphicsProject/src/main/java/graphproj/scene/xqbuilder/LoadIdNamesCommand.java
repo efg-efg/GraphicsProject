3d064f8df7d741cc576c96f7d073999f0f96813d
efg-efg <my_flat_190@yahoo.com>
2013-12-15 00:44:55 +0400

    dobavil klass dlja zagruzki identifikatorov modelej na scene

---
+++ GraphicsProject/src/main/java/graphproj/scene/xqbuilder/LoadIdNamesCommand.java
@@ -0,0 +1,7 @@
+package graphproj.scene.xqbuilder;
+
+/**
+ * Created by alex on 15.12.13.
+ */
+class LoadIdNamesCommand implements Command {
+}
--- GraphicsProject/src/main/java/graphproj/scene/xqbuilder/XQuerySceneBuilder.java
+++ GraphicsProject/src/main/java/graphproj/scene/xqbuilder/XQuerySceneBuilder.java
@@ -6,11 +6,15 @@ import graphproj.scene.*;
  * Created by alex on 14.12.13.
  */
 public class XQuerySceneBuilder  implements SceneBuilder {
-    private Command command;
     private Analyzer analyzer;
     private QueriesExecutor executor;
+    private Command loadIdNames;
+    private void fillCommands(String filename) {
+
+    }
     public XQuerySceneBuilder(String filename) {
         executor = new S9ApiQueriesExecutor("http://www.collada.org/2005/11/COLLADASchema");
+        analyzer = new Analyzer();
     }
     @Override
     public void buildScene() {
