7fcf83b693665480b1adaab0a5e43976b9fef13d
efg-efg <my_flat_190@yahoo.com>
2013-12-14 18:24:21 +0400

    realizoval v klasse Scene interfejs Iterrable<Model>

--- GraphicsProject/src/main/java/graphproj/scene/Scene.java
+++ GraphicsProject/src/main/java/graphproj/scene/Scene.java
@@ -1,8 +1,20 @@
 package graphproj.scene;
 
+import java.util.*;
+
 /**
  * Created by alex on 14.12.13.
  */
-public class Scene {
+public class Scene implements Iterable<Model> {
+    private List<Model> models;
+    public Scene() {
 
+    }
+    public void add(Model model) {
+        models.add(model);
+    }
+    @Override
+    public Iterator<Model> iterator() {
+        return models.iterator();
+    }
 }
